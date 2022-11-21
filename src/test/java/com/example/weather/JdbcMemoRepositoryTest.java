4a2487b6c802fbd03d57e45557ebd0c7f2089083
Cheol Lee <chullee49@naver.com>
2022-11-21 16:15:48 +0900

    1121

---
+++ src/test/java/com/example/weather/JdbcMemoRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.example.weather;
+
+import com.example.weather.domain.Memo;
+import com.example.weather.repository.JDBCMemoRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class JdbcMemoRepositoryTest {
+    @Autowired
+    JDBCMemoRepository jdbcMemoRepository;
+
+    @Test
+    void insertMemoTest() {
+
+        //given
+        Memo newMemo = new Memo(2, "insertMemoTest");
+
+        //when
+        jdbcMemoRepository.save(newMemo);
+
+        //then
+        Optional<Memo> result = jdbcMemoRepository.findById(2);
+        assertEquals(result.get().getText(), "insertMemoTest");
+    }
+
+    @Test
+    void findAllMemoTest() {
+        List<Memo> memoList = jdbcMemoRepository.findAll();
+        System.out.println(memoList);
+        assertNotNull(memoList);
+    }
+}
